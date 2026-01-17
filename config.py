0c4bf512be1faa7533f5f7c675c829ecb8fe066e
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2026-01-17 18:41:48 +0500

    .

--- config.py
+++ config.py
@@ -5,4 +5,12 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
+_raw_admins = os.getenv("ADMINS")
+
+if not _raw_admins:
+    raise ValueError("ADMINS environment variable yo‘q yoki bo‘sh")
+
+ADMINS = [int(x) for x in _raw_admins.split(",") if x.strip().isdigit()]
+
+print("BOT_TOKEN:", BOT_TOKEN)
+print("ADMINS:", ADMINS)
